218a98dfc8e8fb7a8f663625e4cbcd709def1ea9
Nicolas Jacquemin <Nicolas.jcqm@gmail.com>
2018-01-04 22:41:19 +0100

    Recup Parking et insert Bdd

--- app.js
+++ app.js
@@ -44,6 +44,54 @@ const passportConfig = require('./config/passport');
  * Create Express server.
  */
 const app = express();
+const http = require('http');
+const https = require('https');
+
+let urlParking ="https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson";
+let urlWorks = "https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Info_Travaux_Niveau/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson";
+let parkingsArray = new Array();
+let worksArray = new Array();
+
+let conn = mongoose.connection;
+
+conn.collection("parking").remove({});
+
+  https.get(urlParking, (result) => {
+    // Recupere les donnees du parking
+    let body = '';
+    result.on('data', (data) => {
+      body += data;
+    }).on('end', () => {
+      let parkings = JSON.parse(body).features;
+      for (let i = 0; i < parkings.length; i++) {
+
+        let attributes = parkings[i].attributes;
+        let geometry = parkings[i].geometry;
+
+        var parkingInsert = {
+          nom: attributes['NOM'],
+          places: attributes['PLACES'],
+          complet: attributes['COMPLET'],
+          ferme: attributes['FERME'],
+          ouvert: attributes['OUVERT'],
+          capacite: attributes['CAPACITE'],
+          id: attributes['ID'],
+          adresse: attributes['ADRESSE'],
+          date_maj: attributes['DATE_MAJ'],
+          automatique: attributes['AUTOMATIQUE'],
+          taux_occup: attributes['TAUX_OCCUP'],
+          taux_dispo: attributes['TAUX_DISPO'],
+          lien: attributes['LIEN'],
+          x: geometry['x'],
+          y: geometry['y']
+        }
+        // Insertion dans la Bdd
+        conn.collection('parking').insert(parkingInsert);
+      }
+    }).on('error', (e) => {
+      console.log('Error : ' + e.message);
+    });
+  });
 
 /**
  * Connect to MongoDB.
